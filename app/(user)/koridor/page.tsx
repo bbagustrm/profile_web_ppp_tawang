d4df60483d0ef2a6b58e2d90108c756199d8ba3f
Bagus Tri Atmojo <100054889+bbagustrm@users.noreply.github.com>
2025-08-23 08:31:27 +0700

    feat(ui): koridor page

--- app/(user)/koridor/page.tsx
+++ app/(user)/koridor/page.tsx
@@ -9,6 +9,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Eye, Download } from "lucide-react"
+import Image from "next/image";
 // Data untuk carousel media
 const mediaItems = [
     { type: 'video', src: 'https://www.youtube.com/embed/poJsqc81r4c?si=zU0uOtzKJkvuKV4N', title: 'Video Koridor', thumbnail: 'kelompok1.jpg' },
@@ -34,7 +35,7 @@ export default function KoridorPage() {
     const [currentMediaIndex, setCurrentMediaIndex] = useState(0)
 
     // ID file Google Drive
-    const pdfFileId = "19G8_dc0h1WQdcTnohop_Zq6cH9NXe4yE"
+    const pdfFileId = "1_oPn8N-_RKWSydXt8mjT8iEhFCuYBm6f"
 
     // Link view dan download
     const pdfViewUrl = `https://drive.google.com/file/d/${pdfFileId}/view?usp=sharing`
@@ -60,10 +61,10 @@ export default function KoridorPage() {
 
                 <div className="container mx-auto mt-12 text-[#163d4a] ">
                     {/* Main Content Section */}
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[50%_40%] px-4 md:px-0 gap-4 md:gap-8 justify-center items-center mb-12">
+                    <div className="flex flex-col px-4 md:px-0 md:gap-8 justify-center mb-12">
                         {/* Left Side - Media Display Section */}
                         <div>
-                            <Card className="mb-4 ">
+                            <Card className="mb-4">
                                 <CardContent className="p-0">
                                     {/* Main Media Display */}
                                     <div className="relative bg-muted rounded-t-lg overflow-hidden" style={{ aspectRatio: '16/9' }}>
@@ -126,14 +127,15 @@ export default function KoridorPage() {
 
                         {/* Right Side - Scrollable Description */}
                         <div className="flex flex-col">
-                            <Card className="flex-1 mb-4 bg-transparent border-0 shadow-none">
-                                <CardHeader className="p-0 py-4 border-b-2 border-border">
+                            <Card className="flex-1 mb-4 ">
+                                <CardHeader className="border-b-2 border-border">
                                     <CardTitle className="text-[#163d4a] leading-tight ">
                                         <h3 className="leading-normal text-2xl">Redesain Dermaga dan Koridor Pedagang Ikan di P3 Tawang</h3>
                                     </CardTitle>
                                 </CardHeader>
-                                <CardContent className="p-0">
-                                    <ScrollArea className="h-full md:h-[180px] lg:h-[260px] py-4">
+                                <CardContent className="flex flex-col md:flex-row gap-4 md:gap-8">
+                                    {/* Text Section */}
+                                    <ScrollArea className="h-full pt-6 md:basis-3/5 order-2 md:order-1">
                                         <div className="space-y-4 text-sm text-muted-foreground leading-relaxed text-justify">
                                             <p>
                                                 Kuliah Kerja Nyata (KKN) Multidisiplin Universitas kami dilaksanakan di Pelabuhan Perikanan Pantai (PPP) Desa Gempolsewu, Kecamatan Rowosari, Kabupaten Kendal, yang menjadi pusat aktivitas ekonomi sektor perikanan. Tim KKN melakukan observasi langsung terhadap kondisi kawasan bersama pihak pengelola, menemukan bahwa PPP memiliki tiga komponen utama: tempat pelelangan ikan, dua zona kios pedagang ikan (70 kios dekat dermaga dan 48 kios di koridor belakang), serta kios pedagang sembako di pintu masuk. Aktivitas ekonomi cukup dinamis, namun distribusi pengunjung belum merata di seluruh zona kios.
@@ -143,15 +145,39 @@ export default function KoridorPage() {
                                             </p>
                                         </div>
                                     </ScrollArea>
-                                    <div className="flex gap-3 py-4 md:px-4 pb-0 mb-0 border-t-2 border-border items-center">
-                                        <Button onClick={handleViewPDF} variant="outline" className="flex-1 md:flex-none py-4 w-fit">
-                                            <Eye className="w-4 h-4 mr-2" />
-                                            Lihat Detail
-                                        </Button>
-                                        <Button variant="secondary" onClick={handleDownloadPDF} className="flex-1 md:flex-none py-4 w-fit" >
-                                            <Download className="w-4 h-4 mr-2" />
-                                            Download File
-                                        </Button>
+
+                                    {/* Image and Buttons Section */}
+                                    <div className="pt-6 md:basis-2/5 order-1 md:order-2">
+                                        {/* Image Container */}
+                                        <div className="block w-full mx-auto aspect-[4/3] rounded-md overflow-hidden relative mb-4">
+                                            <Image
+                                                src="/preview-panel.png"
+                                                alt="Sertif Image"
+                                                fill
+                                                className="object-fill object-center"
+                                                priority
+                                            />
+                                        </div>
+
+                                        {/* Buttons Container */}
+                                        <div className="flex flex-col sm:flex-row gap-3 py-4 pb-0 mb-0 border-t-2 border-border">
+                                            <Button
+                                                onClick={handleViewPDF}
+                                                variant="outline"
+                                                className="flex-1 sm:flex-none py-3 md:py-4 w-full sm:w-fit"
+                                            >
+                                                <Eye className="w-4 h-4 mr-2" />
+                                                Lihat Detail
+                                            </Button>
+                                            <Button
+                                                variant="secondary"
+                                                onClick={handleDownloadPDF}
+                                                className="flex-1 sm:flex-none py-3 md:py-4 w-full sm:w-fit"
+                                            >
+                                                <Download className="w-4 h-4 mr-2" />
+                                                Download File
+                                            </Button>
+                                        </div>
                                     </div>
                                 </CardContent>
                             </Card>
